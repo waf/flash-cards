e77dd565121f9a1936898b6d8c5601c409b70af6
Will Fuqua <wafuqua@gmail.com>
2016-02-26 21:54:55 +0700

    improve load performance

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ var webpack = require("webpack");
 
 module.exports = {
     context: __dirname + "/src",
-    entry: "./index.js",
+    entry: {
+        app: "./index.js",
+        vendor: ["react", "react-dom", "react-mdl"]
+    },
     output: {
         path: __dirname,
         filename: "build/bundle.js"
@@ -18,5 +21,8 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin("vendor", "build/vendor.bundle.js")
+    ]
 };
